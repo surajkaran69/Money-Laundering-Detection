ceefcb8a2c33bada5288ccc042ed8537acc0efbe
Suraj Karan Vejandla <110095400+surajkaran69@users.noreply.github.com>
2023-03-30 16:53:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import numpy as np 
+import pandas as pd
+import streamlit as st
+import pickle
+loaded_model = pickle.load(open(r"C:\Users\suraj\OneDrive\Desktop\anti money laundering system\model.pkl","rb"))
+#creating afunction for prediction
+def worker_prediction(input_data):
+    
+    input_data_as_numpy_array=np.asarray(input_data)
+    inputdata_reshaped=input_data_as_numpy_array.reshape(1,-1)
+    output=loaded_model.predict(inputdata_reshaped)
+    print(output)
+    if(output[0]==0):
+        return 'This transaction is legit'
+    else:
+        return 'The transaction is fraud'
+    
+def main():
+    st.title("Money Laundering Detection")
+    step                               = st.text_input("Enter Maps of unit time in real world")   
+    type                 = st.text_input("Enter type of money transfer")  
+    amount               = st.text_input("Enter : Amount of transaction in local currency. ") 
+    oldbalanceOrg                = st.text_input("Enter Initial balance before the transaction ")  
+    newbalanceOrig    = st.text_input("Enter  Customer's balance after the transaction. ")  
+    oldbalanceDest          = st.text_input("Enter Initial recipient balance before the transaction. ")  
+    newbalanceDest         = st.text_input("Enter Recipient's balance after the transaction.")  
+    isFlaggedFraud                   = st.text_input("Enter Money laundering or not according our heuristics")
+    
+
+    
+    #code for prediction
+    p= ""
+    if st.button("Result"):
+        p=worker_prediction([step,type,amount,oldbalanceOrg,newbalanceOrig,oldbalanceDest,newbalanceDest,isFlaggedFraud])
+    st.success(p)
+    
+if __name__ == '__main__':
+    main()
+   
